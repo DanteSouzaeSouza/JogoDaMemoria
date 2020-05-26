85aaa56517d59414733a3253b60f4cf17d363d3c
Dante Souza e Souza <lokinmodar@gmail.com>
2020-05-26 17:04:39 -0300

    Initial Commit

--- JogoDaMemoria/Form1.Designer.cs
+++ JogoDaMemoria/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace JogoDaMemoria
 {
-    partial class Form1
+    partial class FrmJogoMem
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,321 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tlpCards = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.tlpCards.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpCards
+            // 
+            this.tlpCards.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.tlpCards.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tlpCards.ColumnCount = 4;
+            this.tlpCards.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCards.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCards.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCards.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCards.Controls.Add(this.label16, 3, 3);
+            this.tlpCards.Controls.Add(this.label15, 2, 3);
+            this.tlpCards.Controls.Add(this.label14, 1, 3);
+            this.tlpCards.Controls.Add(this.label13, 0, 3);
+            this.tlpCards.Controls.Add(this.label12, 3, 2);
+            this.tlpCards.Controls.Add(this.label11, 2, 2);
+            this.tlpCards.Controls.Add(this.label10, 1, 2);
+            this.tlpCards.Controls.Add(this.label9, 0, 2);
+            this.tlpCards.Controls.Add(this.label8, 3, 1);
+            this.tlpCards.Controls.Add(this.label7, 2, 1);
+            this.tlpCards.Controls.Add(this.label6, 1, 1);
+            this.tlpCards.Controls.Add(this.label5, 0, 1);
+            this.tlpCards.Controls.Add(this.label4, 3, 0);
+            this.tlpCards.Controls.Add(this.label3, 2, 0);
+            this.tlpCards.Controls.Add(this.label2, 1, 0);
+            this.tlpCards.Controls.Add(this.label1, 0, 0);
+            this.tlpCards.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpCards.Location = new System.Drawing.Point(0, 0);
+            this.tlpCards.Name = "tlpCards";
+            this.tlpCards.RowCount = 4;
+            this.tlpCards.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCards.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCards.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCards.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCards.Size = new System.Drawing.Size(534, 511);
+            this.tlpCards.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label1.Location = new System.Drawing.Point(2, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 110);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "c";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label2.Location = new System.Drawing.Point(135, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 110);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "c";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label3.Location = new System.Drawing.Point(268, 17);
+            this.label3.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 110);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "c";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label4.Location = new System.Drawing.Point(401, 17);
+            this.label4.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 110);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "c";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label5.Location = new System.Drawing.Point(2, 144);
+            this.label5.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 110);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "c";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label6.Location = new System.Drawing.Point(135, 144);
+            this.label6.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 110);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "c";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label7.Location = new System.Drawing.Point(268, 144);
+            this.label7.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(131, 110);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "c";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label8.Location = new System.Drawing.Point(401, 144);
+            this.label8.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(131, 110);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "c";
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label9.Location = new System.Drawing.Point(2, 271);
+            this.label9.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(131, 110);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "c";
+            // 
+            // label10
+            // 
+            this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label10.Location = new System.Drawing.Point(135, 271);
+            this.label10.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(131, 110);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "c";
+            // 
+            // label11
+            // 
+            this.label11.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label11.Location = new System.Drawing.Point(268, 271);
+            this.label11.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(131, 110);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "c";
+            // 
+            // label12
+            // 
+            this.label12.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label12.Location = new System.Drawing.Point(401, 271);
+            this.label12.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(131, 110);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "c";
+            // 
+            // label13
+            // 
+            this.label13.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label13.Location = new System.Drawing.Point(2, 398);
+            this.label13.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(131, 111);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "c";
+            // 
+            // label14
+            // 
+            this.label14.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label14.Location = new System.Drawing.Point(135, 398);
+            this.label14.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(131, 111);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "c";
+            // 
+            // label15
+            // 
+            this.label15.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label15.Location = new System.Drawing.Point(268, 398);
+            this.label15.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(131, 111);
+            this.label15.TabIndex = 14;
+            this.label15.Text = "c";
+            // 
+            // label16
+            // 
+            this.label16.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Webdings", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label16.Location = new System.Drawing.Point(401, 398);
+            this.label16.Margin = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(131, 111);
+            this.label16.TabIndex = 15;
+            this.label16.Text = "c";
+            // 
+            // FrmJogoMem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.tlpCards);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmJogoMem";
+            this.Text = "Jogo da Memoria";
+            this.tlpCards.ResumeLayout(false);
+            this.tlpCards.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpCards;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- JogoDaMemoria/Form1.cs
+++ JogoDaMemoria/Form1.cs
@@ -10,11 +10,50 @@ using System.Windows.Forms;
 
 namespace JogoDaMemoria
 {
-    public partial class Form1 : Form
+    public partial class FrmJogoMem : Form
     {
-        public Form1()
+
+        //Criando elemento Random para ajudar na criação dos itens do tabuleiro
+        Random random = new Random();
+
+        // Criando e instanciando uma lista de letras para que possa ser usada no tabuleiro
+        // Cada elemento aparece duas vezes na listagem
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
+
+        private void AdicionaIcones()
+        {
+            // O TableLayoutPanel tem 16 labels,
+            // e a lista de ícones tem 16 elementos.
+            // essa função puxa os ícones da lista
+            // e os adiciona aos elementos um a um
+            foreach (Control control in tlpCards.Controls)
+            {
+                // criando um objeto Label e associando-o a um controle de formulário
+                var iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    // Gerando numero aleatório
+                    var randomNumber = random.Next(icons.Count);
+                    // colocando o ícone referente ao número aleatório na label
+                    iconLabel.Text = icons[randomNumber];
+                    // Ajustando a cor do ícone
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    // remove o ícone adicionado da lista de ícones
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+        public FrmJogoMem()
         {
             InitializeComponent();
+
+            AdicionaIcones();
         }
     }
 }
